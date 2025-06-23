66836cd555a9b632162acc3abc342ae0dadd934b
Hans Gerard Karremans <hg.karremans@student.avans.nl>
2025-06-23 12:55:47 +0200

    Rebuild certificate logic

--- The-circle/Presentation/Controllers/CameraController.cs
+++ The-circle/Presentation/Controllers/CameraController.cs
@@ -4,21 +4,28 @@ namespace The_circle.Presentation.Controllers;
 
 public class CameraController : Controller
 {
+    [HttpGet("/Camera/Stream")]
+    public IActionResult Camera()
+    {
+        // 🔑 Read the very same session key
+        var certBytes = HttpContext.Session.Get("TruYouCert");
+        if (certBytes == null)
+            return RedirectToAction("Login", "Auth");
+
+        // Pass base64−no−newlines into the view
+        ViewBag.CertBase64 = Convert.ToBase64String(certBytes);
+        ViewBag.Email      = HttpContext.Session.GetString("TruYouEmail") ?? "Onbekend";
+
+        return View(); // will render Camera.cshtml
+    }
+
     [HttpGet("/Camera/ReceiveStream")]
     public IActionResult ReceiveStream(Guid streamId)
     {
         ViewBag.StreamId = streamId;
-        return View("ReceiveStream"); 
+        return View();
     }
 
     [HttpGet("/Camera/StreamHub")]
-    public IActionResult StreamingHub()
-    {
-        return View("Streaminghub");
-    } 
-    
-    public IActionResult Camera()
-    {
-        return View();
-    }
+    public IActionResult StreamingHub() => View();
 }
